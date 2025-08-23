12db22c80cd4ba6ec056ed63410f3410a879271f
Denis <Hixon10@yandex.ru>
2025-08-23 16:18:20 -0700

    init

---
+++ KustoPlayground.UI/Program.cs
@@ -0,0 +1,20 @@
+using KustoPlayground.Core;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using KustoPlayground.UI;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+        builder.Services.AddScoped<KustoExecutor>();
+
+        await builder.Build().RunAsync();
+    }
+}
